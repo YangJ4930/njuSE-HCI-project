374cb77d7aa23b784165a0c19fde6a9f52ebb47c
杨京 <295945430@qq.com>
2023-09-06 15:00:08 +0800

    finish navbar

---
+++ hci-project/src/app.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Navbar from './navbar';
+
+
+class App extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <React.Fragment>
+                <Navbar />
+                
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default App;
