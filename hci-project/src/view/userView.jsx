1d551ea56ec0f2f42ece187d7a2e250e1ed67b83
charlie <heleyang0311@163.com>
2023-10-10 16:03:42 +0800

    add userVIew

--- hci-project/src/view/userView.jsx
+++ hci-project/src/view/userView.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.js"
-import { faker } from '@faker-js/faker';
+import {faker} from '@faker-js/faker';
+import {Avatar, Card, Col, Row} from "antd";
+import {UserOutlined} from "@ant-design/icons";
+import GamesRepository from "../component/gamesRepository";
+
+
 function UserView(props) {
+    let username = faker.person.firstName() + " " + faker.person.lastName();
+    let level = faker.number.int({min: 1, max: 100});
 
     return (
         <div>
-            <h1>User View</h1>
-            <p>Username: {props.username}</p>
-            <p>Age: {props.age}</p>
+            <Col>
+                <Row>
+                    <Card bordered={false} style={{width: 300}}>
+                        <Avatar size={"large"} icon={<UserOutlined/>}/>
+                        <p>{username}</p>
+                    </Card>
+                    <Card>
+                        <p>等级：{level}</p>
+                    </Card>
+                </Row>
+                <Row>
+                    <GamesRepository/>
+                </Row>
+            </Col>
         </div>
     )
 }
