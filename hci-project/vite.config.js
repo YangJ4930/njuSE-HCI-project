714f93b41eada1dc5e909672f6e555cb2aad8664
charlie <heleyang0311@163.com>
2023-11-27 20:12:55 +0800

    Update file paths and add .idea to .gitignore

--- hci-project/vite.config.js
+++ hci-project/vite.config.js
@@ -1,43 +1,41 @@
 /* eslint-disable */
-import legacyPlugin from '@vitejs/plugin-legacy';
-import * as path from 'path';
-import vitePluginReactJsSupport from 'vite-plugin-react-js-support';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import legacyPlugin from "@vitejs/plugin-legacy";
+import * as path from "path";
+import vitePluginReactJsSupport from "vite-plugin-react-js-support";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 // @see https://cn.vitejs.dev/config/
-export default ({
-  command,
-  mode
-}) => {
+export default ({ command, mode }) => {
   let rollupOptions = {};
-  if (command === 'serve') {
-    rollupOptions.input = ['./src/index.js'];
+  if (command === "serve") {
+    rollupOptions.input = ['./src/index.jsx'];
   }
 
   let optimizeDeps = {};
-  if (command === 'serve') {
+  if (command === "serve") {
     optimizeDeps.entries = false;
   }
 
   let alias = {
-    'react-native': 'react-native-web',
-  }
+    "react-native": "react-native-web",
+  };
 
-  let proxy = {}
+  let proxy = {};
 
   // todo 替换为原有变量
   let define = {
-    'process.env.APP_IS_LOCAL': command === 'serve' ? '"true"' : '"false"',
-    'process.env.REACT_APP_IS_LOCAL': command === 'serve' ? '"true"' : '"false"',
-    'process.env.NODE_ENV': '"development"',
-    'process.env.PUBLIC_URL': '""',
-    'process.env.FAST_REFRESH': 'true',
-  }
+    "process.env.APP_IS_LOCAL": command === "serve" ? '"true"' : '"false"',
+    "process.env.REACT_APP_IS_LOCAL":
+      command === "serve" ? '"true"' : '"false"',
+    "process.env.NODE_ENV": '"development"',
+    "process.env.PUBLIC_URL": '""',
+    "process.env.FAST_REFRESH": "true",
+  };
 
-  let esbuild = {}
+  let esbuild = {};
 
   return {
-    base: './', // index.html文件所在位置
-    root: './', // js导入的资源路径，src
+    base: "./", // index.html文件所在位置
+    root: "./", // js导入的资源路径，src
     resolve: {
       alias,
     },
@@ -47,21 +45,30 @@ export default ({
       proxy,
     },
     build: {
-      target: 'es2015',
-      minify: 'terser', // 是否进行压缩,boolean | 'terser' | 'esbuild',默认使用terser
+      target: "es2015",
+      minify: "terser", // 是否进行压缩,boolean | 'terser' | 'esbuild',默认使用terser
       manifest: false, // 是否产出maifest.json
       sourcemap: false, // 是否产出soucemap.json
-      outDir: 'build', // 产出目录
+      outDir: "build", // 产出目录
       rollupOptions,
     },
     esbuild,
     optimizeDeps,
     plugins: [
       legacyPlugin({
-        targets: ['Android > 39', 'Chrome >= 60', 'Safari >= 10.1', 'iOS >= 10.3', 'Firefox >= 54', 'Edge >= 15'],
-      }), vitePluginReactJsSupport([], {
+        targets: [
+          "Android > 39",
+          "Chrome >= 60",
+          "Safari >= 10.1",
+          "iOS >= 10.3",
+          "Firefox >= 54",
+          "Edge >= 15",
+        ],
+      }),
+      vitePluginReactJsSupport([], {
         jsxInject: true,
-      }), reactRefresh(),
+      }),
+      reactRefresh(),
     ],
     css: {
       preprocessorOptions: {
