15c6b8f11dc81198c7991f3c5338a3112b65371d
heleyang <heleyang0311@163.com>
2023-11-29 14:20:05 +0800

    修正Provider位置

--- hci-project/src/app.jsx
+++ hci-project/src/app.jsx
@@ -1,15 +1,12 @@
 import React, {Component} from 'react';
 import BasicLayout from './layout/basicLayout';
-import {Provider} from "react-redux";
-import store from "./store";
+
 
 function App() {
     return (
-        <Provider store={store}>
-            <React.Fragment>
-                <BasicLayout collapsed={true}/>
-            </React.Fragment>
-        </Provider>
+        <React.Fragment>
+            <BasicLayout collapsed={true}/>
+        </React.Fragment>
     );
 }
 
--- hci-project/src/index.jsx
+++ hci-project/src/index.jsx
@@ -5,13 +5,16 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import App from './app';
 import {BrowserRouter} from 'react-router-dom';
-
+import {Provider} from "react-redux";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </React.StrictMode>
+    <Provider store={store}>
+        <React.StrictMode>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </React.StrictMode>
+    </Provider>
 );
