ea0b16fafdb7e619ee207f569a622e48a19acd50
charlie <heleyang0311@163.com>
2023-12-03 16:38:41 +0800

    fix(news页): merge

--- hci-project/src/view/news/newsView.jsx
+++ hci-project/src/view/news/newsView.jsx
@@ -1,94 +1,21 @@
-import React, { Component } from 'react';
-import { Avatar, List, Space, Carousel } from 'antd';
+import React, {Component} from 'react';
+import {Avatar, List, Space, Carousel, Card} from 'antd';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import { Link } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import axios from '../../axios';
 import {NewHead} from "./component/newHead";
 import {NewsListCard} from "./component/newsListCard";
+import {CommentOutlined, EyeTwoTone, HeartTwoTone} from "@ant-design/icons";
+import {ListNews} from "./component/listNiews";
 
-
-const contentStyle = {
-    margin: 0,
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-};
-
-// const data = Array.from({
-//     length: 23,
-// }).map((_, i) => ({
-//     // ?chapter=${web.id}
-//     href: `/news/content/?document=${i}`,
-//     title: `人机交互为什么是我最喜欢的课的理由 ${i}`,
-//     avatar: `https://xsgames.co/randomusers/avatar.php?g=pixel&key=${i}`,
-//     description:
-//       '人机交互人机交互人机交互',
-//     content:
-//       '人机交互人机交互人机交互人机交互人机交互人机交互人机交互人机交互人机交互人机交互人机交互人机交互人机交互人机交互人机交互人机交互人机交互人机交互人机交互人机交互人机交互v',
-//   }));
-const IconText = ({ text }) => (
-    <Space>
-        {/* {React.createElement(icon)} */}
-        {text}
-    </Space>
-);
-const ListNews = ({ data }) => (
-    <List
-        itemLayout='vertical'
-        size='large'
-        pagination={{
-            onChange: (page) => {
-                console.log(page);
-            },
-            pageSize: 3,
-        }}
-        dataSource={data}
-        footer={
-            <div>
-                <b>ant design</b> footer part
-            </div>
-        }
-        renderItem={(item) => (
-            <List.Item
-                key={item.title}
-                actions={[
-                    <IconText text='156' key='' />,
-                    <IconText text='156' key='' />,
-                    <IconText text='2' key='' />,
-                ]}
-                extra={
-                    <img
-                        width={250}
-                        alt='logo'
-                        src='https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png'
-                    />
-                }
-            >
-                <List.Item.Meta
-                    avatar={<Avatar src={item.avatar} />}
-                    title={<Link to={item.href}>{item.title}</Link>}
-                    description={item.description}
-                />
-                <div
-                    style={{
-                        whiteSpace: 'pre-line',
-                    }}
-                >
-                    {item.description}
-                </div>
-            </List.Item>
-        )}
-    />
-);
 
 function NewsView() {
     const [newsList, setNewsList] = React.useState([]);
 
     React.useEffect(() => {
-        axios.get('/news/content').then((response) => {
+        axios.get('/news/contents').then((response) => {
+            console.log(response);
             setNewsList(response.data);
             console.log(response.data);
         });
@@ -97,21 +24,24 @@ function NewsView() {
     let data = Array.from({
         length: newsList.length,
     }).map((_, i) => ({
-        // ?chapter=${web.id}
-        href: `/news/content/?document=${newsList[i].id}`,
+        id: newsList[i].id,
         title: newsList[i].title,
         avatar: newsList[i].avatar,
         description: newsList[i].description,
         content: newsList[i].content,
+        cover: newsList[i].cover,
+        likes: newsList[i].likes,
+        views: newsList[i].views,
+        comments: newsList[i].comments,
     }));
-        return (
-            <React.Fragment>
-                <NewHead style={{marginBottom: 200}}/>
-                <ListNews data={data}/>
-            </React.Fragment>
+    return (
+        <React.Fragment>
+            <NewHead style={{marginBottom: 200}}/>
+            <ListNews data={data}/>
+        </React.Fragment>
 
 
-        );
+    );
 }
 
 export default NewsView;
