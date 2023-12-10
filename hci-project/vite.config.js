475fc441d8b7b0c5ac0eaf52062ca515dda3f3ab
heleyang <heleyang0311@163.com>
2023-12-10 16:20:26 +0800

    解决冲突

--- hci-project/vite.config.js
+++ hci-project/vite.config.js
@@ -32,50 +32,129 @@ export default ({ command, mode }) => {
 
     let esbuild = {};
 
-    return {
-        base: './', // index.html文件所在位置
-        root: './', // js导入的资源路径，src
-        resolve: {
-            alias
-        },
-        define: define,
-        server: {
-            // 代理
-            proxy
-        },
-        build: {
-            target: 'es2015',
-            minify: 'terser', // 是否进行压缩,boolean | 'terser' | 'esbuild',默认使用terser
-            manifest: false, // 是否产出maifest.json
-            sourcemap: false, // 是否产出soucemap.json
-            outDir: 'build', // 产出目录
-            rollupOptions
-        },
-        esbuild,
-        optimizeDeps,
-        plugins: [
-            legacyPlugin({
-                targets: [
-                    'Android > 39',
-                    'Chrome >= 60',
-                    'Safari >= 10.1',
-                    'iOS >= 10.3',
-                    'Firefox >= 54',
-                    'Edge >= 15'
-                ]
-            }),
-            vitePluginReactJsSupport([], {
-                jsxInject: true
-            }),
-            reactRefresh()
-        ],
-        css: {
-            preprocessorOptions: {
-                less: {
-                    // 支持内联 JavaScript
-                    javascriptEnabled: true
-                }
-            }
-        }
-    };
-};
+  return {
+      base: "./", // index.html文件所在位置
+      root: "./", // js导入的资源路径，src
+      resolve: {
+          alias,
+      },
+      define: define,
+      server: {
+          // 代理
+          proxy,
+      },
+      build: {
+          target: "es2015",
+          minify: "terser", // 是否进行压缩,boolean | 'terser' | 'esbuild',默认使用terser
+          manifest: false, // 是否产出maifest.json
+          sourcemap: false, // 是否产出soucemap.json
+          outDir: "build", // 产出目录
+          rollupOptions,
+      },
+      esbuild,
+
+      plugins: [
+          legacyPlugin({
+              targets: [
+                  "Android > 39",
+                  "Chrome >= 60",
+                  "Safari >= 10.1",
+                  "iOS >= 10.3",
+                  "Firefox >= 54",
+                  "Edge >= 15",
+              ],
+          }),
+          vitePluginReactJsSupport([], {
+              jsxInject: true,
+          }),
+          reactRefresh(),
+      ],
+      css: {
+          preprocessorOptions: {
+              less: {
+                  // 支持内联 JavaScript
+                  javascriptEnabled: true,
+              },
+          },
+          define: define,
+          server: {
+              // 代理
+              proxy,
+          },
+          build: {
+              target: "es2015",
+              minify: "terser", // 是否进行压缩,boolean | 'terser' | 'esbuild',默认使用terser
+              manifest: false, // 是否产出maifest.json
+              sourcemap: false, // 是否产出soucemap.json
+              outDir: "build", // 产出目录
+              rollupOptions,
+          },
+          esbuild,
+
+          plugins: [
+              legacyPlugin({
+                  targets: [
+                      "Android > 39",
+                      "Chrome >= 60",
+                      "Safari >= 10.1",
+                      "iOS >= 10.3",
+                      "Firefox >= 54",
+                      "Edge >= 15",
+                  ],
+              }),
+              vitePluginReactJsSupport([], {
+                  jsxInject: true,
+              }),
+              reactRefresh(),
+          ],
+          css: {
+              preprocessorOptions: {
+                  less: {
+                      // 支持内联 JavaScript
+                      javascriptEnabled: true,
+                  },
+                  define: define,
+                  server: {
+                      // 代理
+                      proxy
+                  },
+                  build: {
+                      target: 'es2015',
+                      minify: 'terser', // 是否进行压缩,boolean | 'terser' | 'esbuild',默认使用terser
+                      manifest: false, // 是否产出maifest.json
+                      sourcemap: false, // 是否产出soucemap.json
+                      outDir: 'build', // 产出目录
+                      rollupOptions
+                  },
+                  esbuild,
+                  optimizeDeps,
+                  plugins: [
+                      legacyPlugin({
+                          targets: [
+                              'Android > 39',
+                              'Chrome >= 60',
+                              'Safari >= 10.1',
+                              'iOS >= 10.3',
+                              'Firefox >= 54',
+                              'Edge >= 15'
+                          ]
+                      }),
+                      vitePluginReactJsSupport([], {
+                          jsxInject: true
+                      }),
+                      reactRefresh()
+                  ],
+                  css: {
+                      preprocessorOptions: {
+                          less: {
+                              // 支持内联 JavaScript
+                              javascriptEnabled: true
+                          }
+                      }
+                  }
+              }
+          }
+      }
+  }
+}
+
