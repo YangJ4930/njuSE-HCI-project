66b6faa0a9967558a25dd3d274b81f4696398af0
jwlnlm <211250112@smail.nju.edu.cn>
2023-12-15 21:16:49 +0800

    更新页面

--- hci-project/src/view/explore/explore_gameRepositoryView.jsx
+++ hci-project/src/view/explore/explore_gameRepositoryView.jsx
@@ -450,11 +450,10 @@ function Panel_Flex(){
     return(
         <Flex id={'filter-activity-discount'}  justify='space-between' align='center' horizontal>
             <h className='filter-panel-choice'>多人竞技</h>
-            <svg t="1702127625015" className="icon" viewBox="0 0 1024 1024" version="1.1"
-                 xmlns="http://www.w3.org/2000/svg" p-id="3478" width="15" height="15">
+            <svg xmlns="http://www.w3.org/2000/svg"  width="15" height="15">
                 <path
                     d="M392.533333 806.4L85.333333 503.466667l59.733334-59.733334 247.466666 247.466667L866.133333 213.333333l59.733334 59.733334L392.533333 806.4z"
-                    fill="#2c2c2c" p-id="3479"></path>
+                    fill="#2c2c2c" ></path>
             </svg>
         </Flex>
     )
