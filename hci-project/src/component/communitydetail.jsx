d1faa6de319bb07b9bc43e697c691d896ad2998a
ntbshishen <1036886215@qq.com>
2023-12-23 16:00:50 +0800

    ia::wq

--- hci-project/src/component/communitydetail.jsx
+++ hci-project/src/component/communitydetail.jsx
@@ -11,41 +11,53 @@ import {
     Tag,
     FloatButton,
     Button,
-    
+
 } from 'antd';
-import { useParams,useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import React from 'react';
 import {
     MessageOutlined,
-    LeftOutlined 
+    LeftOutlined
 } from '@ant-design/icons';
 import { PageContainer } from '@ant-design/pro-components';
 import { ProList } from '@ant-design/pro-components';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import CommentPost from '../component/commentPost';
 import axios from '../axios'
+import { marked } from 'marked';
+
 const Communitydetail = function Comunitydetail() {
+    marked.setOptions({
+        renderer: new marked.Renderer(),
+        gfm: true,
+        tables: true,
+        breaks: true,
+        pedantic: false,
+        sanitize: true,
+        smartLists: true,
+        smartypants: false,
+    });
     const navigate = useNavigate()
     const { Meta } = Card;
     const [load, setLoad] = useState(false);
     const [loading, setLoading] = useState(false);
     const [detail, setDetail] = useState({
-        id:"",
-        author:"",
-        context:"",
-        imageUrl:"",
-        title:"",
+        id: "",
+        author: "",
+        context: "",
+        imageUrl: "",
+        title: "",
     });
-    const [imageUrl,setImageUrl]=useState([])
-    const [commentDetail,setCommentDetail]=useState([])
-    const [pageNumber,setPageNumber]=useState(0);
+    const [imageUrl, setImageUrl] = useState([])
+    const [commentDetail, setCommentDetail] = useState([])
+    const [pageNumber, setPageNumber] = useState(0);
     const { communityId } = useParams();
-    const back = ()=>{
+    const back = () => {
         navigate(-1)
     }
     const loadMoreData = () => {
-        console.log("nb: "+pageNumber)
+        console.log("nb: " + pageNumber)
         if (loading) {
             return;
         }
@@ -53,8 +65,8 @@ const Communitydetail = function Comunitydetail() {
         axios.get(`/community/findAllComment/${pageNumber}/${communityId}`)
             .then((response) => {
                 console.log(response);
-                setCommentDetail([...response.data,...commentDetail])
-                const number=pageNumber+1;
+                setCommentDetail([...response.data, ...commentDetail])
+                const number = pageNumber + 1;
                 setPageNumber(number);
                 setLoading(false);
             })
@@ -63,20 +75,20 @@ const Communitydetail = function Comunitydetail() {
                 setLoading(false);
             })
     };
-        // const CNM=()=>{
-        //     console.log("reset")
-        //     setCommentDetail([])
-        //     setPageNumber(0);
-        //     loadMoreData();
-        // }
+    // const CNM=()=>{
+    //     console.log("reset")
+    //     setCommentDetail([])
+    //     setPageNumber(0);
+    //     loadMoreData();
+    // }
     const findCommunityDetail = () => {
         axios.get(`/community/findCommunityDetail/${communityId}`)
             .then((response) => {
                 //console.log(response);
-                var imageUrls=response.data.imageUrl;
-                var shuzu=imageUrls.split(',')
-                setImageUrl(shuzu.slice(0,shuzu.length-1))
-               // console.log(imageUrl);
+                var imageUrls = response.data.imageUrl;
+                var shuzu = imageUrls.split(',')
+                setImageUrl(shuzu.slice(0, shuzu.length - 1))
+                // console.log(imageUrl);
                 setDetail(response.data)
             })
             .catch((err) => {
@@ -90,133 +102,130 @@ const Communitydetail = function Comunitydetail() {
     }, []);
     return (
         <>
-        <Button icon={<LeftOutlined />}
-        
-        type="link"
-        onClick={()=>{
-            back()
-        }}></Button>
-        <PageContainer title={detail.title}>
-            <div
-                style={{
-                    width: '100%',
-                    display: 'flex',
-                    justifyContent: 'center',
-                }}
-            >
+            <Button icon={<LeftOutlined />}
+
+                type="link"
+                onClick={() => {
+                    back()
+                }}></Button>
+            <PageContainer title={detail.title}>
+                <div
+                    style={{
+                        width: '100%',
+                        display: 'flex',
+                        justifyContent: 'center',
+                    }}
+                >
                     <Image.PreviewGroup
                         items={imageUrl}
                     >
                         <Image
-                         height={400}
-                         src={imageUrl[0]}>
+                            height={400}
+                            src={imageUrl[0]}>
                         </Image>
                     </Image.PreviewGroup>
 
-            </div>
-            <Divider plain></Divider>
-            <Row>
-                <Card
-                    bordered={false}
-                    hoverable
-                    style={{
-                        width: '300px',
-                    }}
-                >
-                    <Meta
-                        avatar={
-                            <>
-                                <Avatar
-                                    size={64}
-                                    src='https://xsgames.co/randomusers/avatar.php?g=pixel'
-                                />
-                            </>
-                        }
-                        title={
-                            <Row>
-                                <div>{detail.author}</div>
-                                <Tag
-                                    color='#2db7f5'
-                                    style={{
-                                        marginLeft: 10,
-                                    }}
-                                >
-                                    作者
-                                </Tag>
-                            </Row>
-                        }
-                        description='杨静nb'
-                    />
-                </Card>
-            </Row>
-            <Divider plain orientation='left'>
-                正文
-            </Divider>
-            <div
-                style={{
-                    whiteSpace: 'pre-line',
-                    fontSize: '16px',
-                }}
-            >
-                {detail.context}
-            </div>
-
-            <br></br>
-            <InfiniteScroll
-                infinite-scroll-disabled={false}
-                dataLength={commentDetail.length}
-                next={loadMoreData}
-                hasMore={commentDetail.length%4==0}
-                loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
-                endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
-                scrollableTarget='scrollableDiv'
-            >
-                <Row justify='space-between'></Row>
-                <Divider orientation='left'>
-                    {' '}
-                    <Row>
-                        <div
-                            style={{
-                                fontWeight: 'bold',
-                            }}
-                        >
-                            评论
-                            <MessageOutlined></MessageOutlined>
-                        </div>
-                    </Row>
-                </Divider>
-                <ProList
-                    size='small'
-                    itemLayout='vertical'
-                    rowKey='id'
-                    dataSource={commentDetail}
-                    split={false}
-                    renderItem={(item) => {
-                        return (
-                            <List.Item>
-                                <Card
-                                    hoverable
-                                    style={{ background: 'rgba(219,225,240,0.3)' }}
-                                    bordered={false}
-                                >
-                                    <List.Item.Meta
-                                        avatar={<Avatar src={item.user.avatarUrl} />}
-                                        title={<a href='https://ant.design'>{item.user.username}</a>}
-                                        description={
-                                            <div style={{ fontSize: 10, marginTop: -10 }}>
-                                                今天 11:21
-                                            </div>
-                                        }
+                </div>
+                <Divider plain></Divider>
+                <Row>
+                    <Card
+                        bordered={false}
+                        hoverable
+                        style={{
+                            width: '300px',
+                        }}
+                    >
+                        <Meta
+                            avatar={
+                                <>
+                                    <Avatar
+                                        size={64}
+                                        src='https://xsgames.co/randomusers/avatar.php?g=pixel'
                                     />
-                                    <div
+                                </>
+                            }
+                            title={
+                                <Row>
+                                    <div>{detail.author}</div>
+                                    <Tag
+                                        color='#2db7f5'
                                         style={{
-                                            marginLeft: 40,
+                                            marginLeft: 10,
                                         }}
                                     >
-                                        {item.comment}
-                                    </div>
+                                        作者
+                                    </Tag>
+                                </Row>
+                            }
+                            description='杨静nb'
+                        />
+                    </Card>
+                </Row>
+                <Divider plain orientation='left'>
+                    正文
+                </Divider>
+                <div
+                    id='content'
+                    //className='article-detail'
+                    style={{ fontSize: 20, backgroundColor: 'white' }}
+                    dangerouslySetInnerHTML={{ __html: marked(detail.context.join('\n')) }}
+                />
+                <br></br>
+                <InfiniteScroll
+                    infinite-scroll-disabled={false}
+                    dataLength={commentDetail.length}
+                    next={loadMoreData}
+                    hasMore={commentDetail.length % 4 == 0}
+                    loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
+                    endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
+                    scrollableTarget='scrollableDiv'
+                >
+                    <Row justify='space-between'></Row>
+                    <Divider orientation='left'>
+                        {' '}
+                        <Row>
+                            <div
+                                style={{
+                                    fontWeight: 'bold',
+                                }}
+                            >
+                                评论
+                                <MessageOutlined></MessageOutlined>
+                            </div>
+                        </Row>
+                    </Divider>
+                    <ProList
+                        size='small'
+                        itemLayout='vertical'
+                        rowKey='id'
+                        dataSource={commentDetail}
+                        split={false}
+                        renderItem={(item) => {
+                            return (
+                                <List.Item>
+                                    <Card
+                                        hoverable
+                                        style={{ background: 'rgba(219,225,240,0.3)' }}
+                                        bordered={false}
+                                    >
+                                        <List.Item.Meta
+                                            avatar={<Avatar src={item.user.avatarUrl} />}
+                                            title={<a href='https://ant.design'>{item.user.username}</a>}
+                                            description={
+                                                <div style={{ fontSize: 10, marginTop: -10 }}>
+                                                    今天 11:21
+                                                </div>
+                                            }
+                                        />
+                                        <div
+                                            style={{
+                                                marginLeft: 40,
+                                            }}
+                                        >
+                                            {item.comment}
+                                        </div>
 
-                                    {/* <img
+                                        {/* <img
                   style={{
                     marginLeft: 60,
                   }}
@@ -224,16 +233,16 @@ const Communitydetail = function Comunitydetail() {
                   alt="logo"
                   src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
                 /> */}
-                                </Card>
-                            </List.Item>
-                        );
-                    }}
-                ></ProList>
-            </InfiniteScroll>
-        </PageContainer>
-        <FloatButton.Group>
-            <FloatButton description={<CommentPost communityId={communityId} ></CommentPost>}></FloatButton>
-            <FloatButton.BackTop className="backtop" />
+                                    </Card>
+                                </List.Item>
+                            );
+                        }}
+                    ></ProList>
+                </InfiniteScroll>
+            </PageContainer>
+            <FloatButton.Group>
+                <FloatButton description={<CommentPost communityId={communityId} ></CommentPost>}></FloatButton>
+                <FloatButton.BackTop className="backtop" />
             </FloatButton.Group>
         </>
     );
